fca87dc5e2de2bf7fa5ad7b4324eb1d5cf586734
tianyi-xia1 <tianyi8@student.ubc.ca>
2024-12-12 18:26:36 -0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="frenchlearning",
+    version="0.1.0",
+    author="Tianyi Xia, Litao Zheng",
+    author_email="kevinhsia6@gmail.com, zhenglitao0305@gmail.com",
+    description="A Python package for learning French, including memorization and quizzing modules.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tianyi-xia1/project-step-3.git",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Education",
+    ],
+    python_requires=">=3.9",
+    install_requires=[],
+    include_package_data=True,
+)
