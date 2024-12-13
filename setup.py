234e70c34fff455a8bf39a6447afa0a03f6c706b
tianyi-xia1 <tianyi8@student.ubc.ca>
2024-12-12 19:35:37 -0800

    minor

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="frenchlearning",
-    version="0.1.0",
+    version="0.1.1",
     author="Tianyi Xia, Litao Zheng",
     author_email="kevinhsia6@gmail.com, zhenglitao0305@gmail.com",
     description="A Python package for learning French, including memorization and quizzing modules.",
